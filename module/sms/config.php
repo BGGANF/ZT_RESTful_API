898577cdca57b3cf513fbae6b1c691ce1939ab32
wing <183862684@qq.com>
2017-03-22 00:20:50 +0800

    init

--- module/sms/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-$config->sms = new stdClass();
-$config->sms->outTime = 5 * 60;
-$config->sms->errorTime = 5;
--- module/token/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$config->token = new stdClass();
-$config->token->expire = 36000;
-$config->token->isSingleLogin = false;
